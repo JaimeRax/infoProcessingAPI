886545ebac52abed447ce3c3aa14a268d6290c61
JaimeRax <jaimecaal60@gmail.com>
2024-09-23 14:12:47 -0600

    fix(test): add route general to .env and call file

--- app/lectorDPI/test.py
+++ app/lectorDPI/test.py
@@ -1,8 +1,13 @@
+from re import template
 import cv2
 import os
+from dotenv import load_dotenv
 
+load_dotenv() # load the .env file 
+base_path = os.getenv('PATH_INFO')
 
-imgQ = cv2.imread('plantilla.png')
+template_image = os.path.join(base_path, 'app/lectorDPI/plantilla.png')
+imgQ = cv2.imread(template_image)
 h,w,c = imgQ.shape
 imgQ = cv2.resize(imgQ, (w//3,h//3))
 
@@ -10,8 +15,8 @@ orb = cv2.ORB_create(4000)
 kp1, des1 = orb.detectAndCompute(imgQ,None)
 # impKp1 = cv2.drawKeypoints(imgQ, kp1, None)
 
-path = '/home/jaime/Documents/university/infoProcessingAPI/media/'
-myPiclist = os.listdir(path)
+path_files = os.path.join(base_path, 'media/')
+myPiclist = os.listdir(path_files)
 print(myPiclist)
 
 
