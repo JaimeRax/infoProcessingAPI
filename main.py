8ac74fac0d5c6868cf40c652fef2aec4ab7535d9
JaimeRax <jaimecaal60@gmail.com>
2024-10-07 22:06:21 -0600

    ci(facialRecognition): remove code to compare facial

--- main.py
+++ main.py
@@ -2,14 +2,12 @@
 from flask import Flask, jsonify, request
 
 #Importacion de rutas
-from app.facialRecognition.routes import facialRecognition
 from app.lectorDPI.routes import lectorDPI
 
 #Instanciando el objeto para nuestra app
 app = Flask(__name__)
 
 #Definicion de rutas
-app.register_blueprint(facialRecognition, url_prefix='/api/facial_recognition')
 app.register_blueprint(lectorDPI, url_prefix='/api/lectorDPI/')
 
 
